6b1a4747fdae90164e639883622299c8363acbb7
Fueredoriku <fueredoriku@gmail.com>
2026-01-31 22:20:27 +0100

    feat: the levvel for reals

--- Assets/Scripts/Masks/HideMask.cs
+++ Assets/Scripts/Masks/HideMask.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class RevealMask : MonoBehaviour, Mask
+public class HideMask : MonoBehaviour, Mask
 {
     [SerializeField]
     private LayerMask affectedObjectLayer;
@@ -9,13 +9,13 @@ public class RevealMask : MonoBehaviour, Mask
     {
         foreach (var item in FindObjectsByType<GameObject>(FindObjectsInactive.Include, sortMode: FindObjectsSortMode.None))
             if ((affectedObjectLayer.value & (1 << item.layer)) != 0)
-                item.SetActive(true);
+                item.SetActive(false);
     }
 
     public void OnUnequipped()
     {
         foreach (var item in FindObjectsByType<GameObject>(FindObjectsInactive.Include, FindObjectsSortMode.None))
             if ((affectedObjectLayer.value & (1 << item.layer)) != 0)
-                item.SetActive(false);
+                item.SetActive(true);
     }
 }
