4e8916f79295fd83dcd7e6245ee7124f081bd56f
Anders <6058745+ddabble@users.noreply.github.com>
2026-02-01 12:51:12 +0100

    Improved equipped+held mask UI
    The held mask is now at the bottom of the screen, facing the camera,
    and the equipped mask is mounted on a spinning bust in the bottom left
    corner of the screen.

--- Assets/Scripts/Bust.cs
+++ Assets/Scripts/Bust.cs
@@ -5,4 +5,16 @@ public class Bust : MonoBehaviour
     [SerializeField]
     private Transform maskMountPoint;
     public Transform MaskMountPoint => maskMountPoint;
+
+    public void EquipMask(Mask mask)
+    {
+        var maskTransform = mask.GameObject.transform;
+        maskTransform.SetParent(transform);
+
+        maskTransform.position = MaskMountPoint.position;
+        // Make the mask face away from the bust
+        maskTransform.LookAt(
+            maskTransform.position + transform.forward, transform.up
+        );
+    }
 }
