e3509a3f7473e5a391ed8787a4bc1c309a6c0b6a
Tore Bergebakken <toberge@stud.ntnu.no>
2026-02-01 16:54:29 +0100

    ADD SOUNDS OR STH

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -1,13 +1,23 @@
-using System;
+using FMODUnity;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(StudioEventEmitter))]
 public class NextScene : MonoBehaviour
 {
     [SerializeField]
     private string nextSceneName;
+
+    private StudioEventEmitter emitter;
+
+    private void Awake()
+    {
+        emitter = GetComponent<StudioEventEmitter>();
+    }
+
     private void OnTriggerEnter()
     {
+        emitter.Play();
         SceneManager.LoadScene(nextSceneName);
     }
 }
