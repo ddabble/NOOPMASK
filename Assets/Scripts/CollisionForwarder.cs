f5f87d32be6a1dc020038fb34201065ca97736b6
Fueredoriku <fueredoriku@gmail.com>
2026-02-01 14:13:55 +0100

    feat: level3

---
+++ Assets/Scripts/CollisionForwarder.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class CollisionForwarder : MonoBehaviour
+{
+    private Rigidbody body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision c)
+    {
+        if (Pendulum.Singleton.transform.GetChild(0).gameObject == c.gameObject)
+        {
+            StartCoroutine(WaitAndPush(Pendulum.Singleton.DispenseCount));
+            Pendulum.Singleton.OnCollision(c);
+        }
+    }
+    
+    private IEnumerator WaitAndPush(int times)
+    {
+        yield return null;
+        var force = times == 1 && Pendulum.Singleton.Sign < 0 ? -7f : -15f;
+        body.AddForce(new Vector3(0f, 0f, force * Pendulum.Singleton.Sign * Mathf.Min(times, 2f)), ForceMode.Impulse);
+    }
+}
